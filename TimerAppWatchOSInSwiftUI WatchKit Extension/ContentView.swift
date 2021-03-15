29c9e0e8d054751cae2383b0a7d6f5aa9eeae0be
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-15 11:05:28 +0200

    Initial Commit

---
+++ TimerAppWatchOSInSwiftUI WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerAppWatchOSInSwiftUI WatchKit Extension
+//
+//  Created by ramil on 15.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerAppWatchOSInSwiftUI WatchKit Extension/TimerAppWatchOSInSwiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  TimerAppWatchOSInSwiftUIApp.swift
+//  TimerAppWatchOSInSwiftUI WatchKit Extension
+//
+//  Created by ramil on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppWatchOSInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
